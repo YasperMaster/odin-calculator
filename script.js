35833c92f3315c6966e10ab7b0d93a88a7047929
Tomas Yasparra <tomasyas@gmail.com>
2024-06-26 07:38:22 -0300

    add calculator interface

---
+++ script.js
@@ -0,0 +1,23 @@
+let number1;
+let number2;
+let operator;
+
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+function subtract(n1, n2) {
+  return n1 - n2;
+}
+
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+
+function divide(n1, n2) {
+  return n1 / n2;
+}
+
+function operate(number1, number2, operator) {
+  return;
+}
